7a7dbc34ace8246d7fb6adebfb04c40833ac6748
jaimitog7 <jaime.garcia1@virginiogomez.cl>
2019-01-23 03:58:57 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Usuario } from './models/usuario';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'crud-angular';
-}
+  usuarioArray: Usuario[] = [
+    { id: 1, nombre: "Jaime", cargo: "Informatico" },
+    { id: 2, nombre: "Luka", cargo: "Informatico" }
+  ];
+
+  createUsuario: Usuario = new Usuario();
+
+  agregar() {
+    if (this.createUsuario.id === 0) {
+      this.createUsuario.id = this.usuarioArray.length + 1;
+      this.usuarioArray.push(this.createUsuario);
+    }
+    this.createUsuario = new Usuario();
+  }
+
+  editar(usuario: Usuario) {
+    this.createUsuario = usuario;
+  }
+
+  eliminar() {
+    if (confirm('Eliminar, estas Seguro?')) {
+      this.usuarioArray = this.usuarioArray.filter(x => x != this.createUsuario);
+      this.createUsuario = new Usuario();
+    }
+  }
+
+} 
---
+++ src/app/models/usuario.ts
@@ -0,0 +1,5 @@
+export class Usuario {
+    id: number = 0;
+    nombre: string;
+    cargo: string;
+}
